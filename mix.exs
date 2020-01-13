3af86483cbbca99295f88d152f2cefdd8259a89e
Ian Luites <IanLuites@users.noreply.github.com>
2020-01-13 15:40:51 +0100

    Release v0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Status.Mixfile do
   def project do
     [
       app: :status,
-      version: "0.1.0",
+      version: "0.1.1",
       description: "Application status reports",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
